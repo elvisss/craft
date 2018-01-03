a072e3748ba541e0e526454b583001d1c5476256
Elvis Saavedra <esaavedrabs@belatrixsf.com>
2018-01-02 22:15:16 -0500

    fixed bugs

--- js/nosotros.js
+++ js/nosotros.js
@@ -1,5 +1,5 @@
 $(function(){
   $('.navbar-custom li:nth-child(2)').addClass('current-menu-item');
-  $('.navbar-custom').addClass('navbar-dark');
+  $('.navbar').addClass('navbar-dark');
   $('.menu-logo').addClass('menu-inverse');
 });
